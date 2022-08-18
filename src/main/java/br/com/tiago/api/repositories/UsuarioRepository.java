d7b5025c6503949eb80967764e48bc3e3d426137
Tiago Oliveira <tiagodigital22@gmail.com>
2022-08-18 17:55:55 -0300

    Interface Repository

---
+++ src/main/java/br/com/tiago/api/repositories/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package br.com.tiago.api.repositories;
+
+import br.com.tiago.api.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+}
